5fd15c3da2a9b8169b6dad787970cc4c240f0e77
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-15 09:12:03 +0200

    added all packages

---
+++ src/OpenAPISwaggerUI/OpenAPISwaggerUI/ExtensionsOpenAPISwaggerUI.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Scalar.AspNetCore;
+using NetCore2BlocklyNew;
+namespace OpenAPISwaggerUI;
+
+public static class ExtensionsOpenAPISwaggerUI
+{
+    public static WebApplication UseOpenAPISwaggerUI(this WebApplication app)
+    {
+        //goto /swagger-Swashbuckle
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/openapi/v1.json", "v1");
+            options.RoutePrefix = "swagger-Swashbuckle";
+        });
+        //scalar
+        //goto scalar/v1
+        app.MapScalarApiReference(opt =>
+        {
+
+        });
+        //redoc
+        //goto /api-docs
+        app.UseReDoc(options =>
+        {
+            options.SpecUrl("/openapi/v1.json");
+        });
+
+        //goto /nswag-swagger
+        app.UseSwaggerUi(options =>
+        {
+            options.DocumentPath = "/openapi/v1.json";
+            options.Path = "/swagger-nswag";
+        });
+
+        app.UseBlocklyUI(app.Environment);
+        app.UseBlocklyAutomation();
+        return app;
+    }
+
+
+}
