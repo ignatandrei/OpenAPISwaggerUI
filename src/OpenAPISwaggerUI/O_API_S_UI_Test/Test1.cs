6e989ce511d09b236ed8c67bdb7cc9f0652e85cb
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-16 08:08:33 +0200

    integrate with webapplication factory

--- src/OpenAPISwaggerUI/O_API_S_UI_Test/Test1.cs
+++ src/OpenAPISwaggerUI/O_API_S_UI_Test/Test1.cs
@@ -1,24 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Playwright;
 
 namespace O_API_S_UI_Test;
 
 [TestClass]
 public sealed class Test1
 {
-    static WebApplicationFactory<Program> factory;
+    static CustomWebApplicationFactory factory;
     [AssemblyInitialize]
     public static void AssemblyInit(TestContext context)
     {
-        factory = new WebApplicationFactory<Program>();
+        factory = new ();
+        var exitCode = Microsoft.Playwright.Program.Main(new[] { "install" });
+        if (exitCode != 0)
+        {
+            throw new Exception($"Playwright exited with code {exitCode}");
+        }
+        var url = factory.ServerAddress;
+        Console.WriteLine($"Server address: {url}");
     }
 
     [TestMethod]
-    public async Task TestMethod1()
+    public async Task TestWeHaveRealWebServer()
     {
-        using var client = factory.CreateClient();
+        
+        using var client = factory.CreateDefaultClient();
         var response = await client.GetAsync("/weatherforecast");
         response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
-        Assert.IsTrue(content.Contains("Freezing"));
+        var baseAddress = factory.ServerAddress;
+        if(!baseAddress.EndsWith("/"))
+        {
+            baseAddress += "/";    
+        }
+        using var playwright = await Playwright.CreateAsync();
+        var request = await playwright.APIRequest.NewContextAsync();
+            
+        var response1 = await request.GetAsync(baseAddress+"weatherforecast");
+        Assert.IsTrue(response1.Ok);
+
+
     }
+    [TestMethod]
+    public async Task TestIntegrationWorks()
+    {
+
+        var baseAddress = factory.ServerAddress;
+        if (!baseAddress.EndsWith("/"))
+        {
+            baseAddress += "/";
+        }
+        using var playwright = await Playwright.CreateAsync();
+        var request = await playwright.APIRequest.NewContextAsync();
+
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var context = await browser.NewContextAsync(new()
+        {
+            //RecordVideoDir = curDirVideos
+        });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync(baseAddress + "swagger");
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "swagger.png" });
+
+
+    }
+
 }
