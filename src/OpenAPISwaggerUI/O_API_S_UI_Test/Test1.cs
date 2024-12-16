6a0293d6db2e0ec37c00f8ee525dbdb5d1c46ffa
Andrei Ignat <ignatandrei@yahoo.com>
2024-12-16 07:41:12 +0200

    first test

---
+++ src/OpenAPISwaggerUI/O_API_S_UI_Test/Test1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace O_API_S_UI_Test;
+
+[TestClass]
+public sealed class Test1
+{
+    static WebApplicationFactory<Program> factory;
+    [AssemblyInitialize]
+    public static void AssemblyInit(TestContext context)
+    {
+        factory = new WebApplicationFactory<Program>();
+    }
+
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+        using var client = factory.CreateClient();
+        var response = await client.GetAsync("/weatherforecast");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.IsTrue(content.Contains("Freezing"));
+    }
+}
